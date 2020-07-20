30bc23ecedb80902927d6ff5b25d57d6a424bc35
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-20 09:15:26 -0500

    all tests pass, returns error if neg

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+  let output = '';
+  if (repeats < 0) {
+    return 'ERROR'
+  } else {
+    for(let i=0; i < repeats; i++){
+      output += str;
+    }
+    return output;
+  }
+  
 }
 
 module.exports = repeatString
