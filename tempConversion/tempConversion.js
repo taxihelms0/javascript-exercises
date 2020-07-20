14681c4b608472cf4e145728f53dc78283634bf4
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-20 17:51:49 -0500

    rounding to decimal added to temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+/* 
+  [°F] = [°C] × ​9⁄5 + 32	
+  [°C] = ([°F] − 32) × ​5⁄9
+*/
 
+const roundToDecimal = function(num){
+  return Math.round((num + Number.EPSILON) * 10) / 10; // why does this work
 }
 
-const ctof = function() {
+const ftoc = function(f) {
+  return roundToDecimal((f - 32) * (5/9));
+}
 
+const ctof = function(c) {
+  return roundToDecimal(c * (9/5) + 32);
 }
 
 module.exports = {
