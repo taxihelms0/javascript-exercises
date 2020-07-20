2fb3ccefe52e564f38816c4ab14e687ee7a58c9f
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-20 13:56:57 -0500

    successful palindrome checks

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+  // remove punctuation and save in new string
+  let newString = str.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+  // counter vars
+  let downCounter = newString.length - 1;
+  let upCounter = 0;
+  // compare first and last letter moving towards center
+  // works for odd and even lengths. for odd lengths, checks middle char against itself
+  while(downCounter >= upCounter){
+    if(newString[upCounter] === newString[downCounter]){
+      // increment counters
+      upCounter++;
+      downCounter--;
+    } else {
+      // found a mismatch
+      console.log(`${str} is NOT a palindrome!`);
+      return false;
+    }
+    // all letters check out. Therefore str is a palindrome
+    console.log(`${str} is a palindrome`);
+    return true;
+  }
+  
 }
-
 module.exports = palindromes
