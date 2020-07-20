a36487a5b139ff58b5f1ea0767681a6948e85ee7
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-20 17:36:10 -0500

    figures out leapyears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // if year is  divisible by 4
+  if (year % 4 === 0){  
+    // if year is divisible by 100, but not 400
+    if (year % 100 === 0 && year % 400 !== 0){ 
+      return false;
+    }
+    return true;
+  } else{
+    return false;
+  }
+  
 }
 
 module.exports = leapYears
