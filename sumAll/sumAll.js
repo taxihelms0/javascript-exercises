380b0b663d801d4c0929d4dce9d563ed391a15a9
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-20 14:43:54 -0500

    all tests passed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+  let upperNum;
+  let lowerNum;
+  let total = -1; // initialize total to negative to throw error as default
+  
+  // set lower and upper values based on which is larger
+  if(firstNum > secondNum){
+    lowerNum = secondNum;
+    upperNum = firstNum;
+    total = lowerNum; // start total at lower number
+  } else if (secondNum > firstNum){
+    lowerNum = firstNum;
+    upperNum = secondNum;
+    total = lowerNum; // start total at lower number
+  } 
+  // if total is negative throw error or either number is an array or string
+  if (total < 0 || firstNum.length > 1 || secondNum.length > 1) {
+    return 'ERROR';
+  }
+  else {
+    while (lowerNum < upperNum) {
+      // add lowerNumer + 1 to the total
+      total += lowerNum + 1; 
+      lowerNum++;
+    }
+    return total
+  }
+  
+  
 }
 
 module.exports = sumAll
