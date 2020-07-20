dd3870e991f4400f8f695c08211fd8e7642eaf97
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-20 16:38:59 -0500

    using indexOf and splice

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  let newArr = [];
+  let index;
+  // console.log(args);
+  
+  for(let i=0; i < args.length; i++){
+    index = arr.indexOf(args[i]);
+    if(index > -1){
+      
+      arr.splice(index, 1);
+    }
+  }
+  console.log(arr);
+  return arr;
 }
 
 module.exports = removeFromArray
