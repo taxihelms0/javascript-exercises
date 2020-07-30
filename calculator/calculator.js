db5f0a34dd95d5742d654725eda1dadd010f6ac7
Alex Ian Smith <taxihelms0@gmail.com>
2020-07-30 16:52:51 -0500

    calc & palindromes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,33 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (vals) {
+  return vals.reduce((a, b) => (a + b), 0);
 }
 
-function multiply () {
-	
+function multiply (vals) {
+	return vals.reduce((a, b) => (a * b), 1); // start w/ 1 bc anything * 0 == 0 
 }
 
-function power() {
-	
+function power(x, n) {
+	if( n === 1) {
+    return x;
+  } else {
+    return x * power(x, n - 1);
+  }
 }
 
-function factorial() {
-	
+function factorial(a) {
+  if(a === 0){
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 }
 
 module.exports = {
