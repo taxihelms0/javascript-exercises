8b5e9d46988aedf27b9996a27c25f990c361291d
Alex Ian Smith <taxihelms0@gmail.com>
2020-08-01 16:00:46 -0500

    fibonacci generator working

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
 
+const fibonacci = function(num) {
+  if (num == 0){
+      return 0;
+  }
+  if (num == 1) {
+    return 1;
+  }  
+  if(num >= 0) {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+  return 'OOPS';
 }
 
 module.exports = fibonacci
