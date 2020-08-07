28075d4d297099b9b192e18ee21496211916e150
Alex Ian Smith <taxihelms0@gmail.com>
2020-08-07 13:14:17 -0500

    caesar & oldest

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,28 @@
-const caesar = function() {
-
+const caesar = function(str, shift) {
+  let newStrArray = [];
+  shift = shift % 26;
+  // const strArray = str.split('');
+  for(let i=0; i < str.length; i++) {
+    const char = str.charAt(i);
+    // ascii letter range 'A' 65 -> 'Z' 90 && 'a' 97 -> 'z' 122
+    if(char.match(/^[A-Za-z]+$/)){ // if char is a letter
+      newStrArray[i] = char.charCodeAt(0) + shift; // apply shift
+      // constrain to alphabet if it's above 'z' or 'Z'
+      if ((char.match(/^[a-z]+$/) && newStrArray[i] > 122) || (char.match(/^[A-Z]+$/) && newStrArray[i] > 90)) {
+        newStrArray[i] -= 26;
+      }
+      // constrain to alphabet if it's below 'a' or 'A'
+      else if ((char.match(/^[a-z]+$/) && newStrArray[i] < 97) || (char.match(/^[A-Z]+$/) && newStrArray[i] < 65)){
+        newStrArray[i] += 26;
+      }
+      // convert code back to char
+      newStrArray[i] = String.fromCharCode(newStrArray[i]);
+    } else { // char is punctuation
+      newStrArray[i] = char;
+    }
+  }
+  // join chars in array to form string
+  return newStrArray.join('')
 }
 
 module.exports = caesar
--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+  const theDate = new Date();
+  const year = theDate.getFullYear();
+  
+  // sort by age
+  const oldest = people.sort((prev, curr) => {
+    // if person is still alive set "death" to current year
+    if (!curr.yearOfDeath){ 
+      curr.yearOfDeath = year;
+    }
+    // push older ages to top, younger to bottom (1, -1)
+    return ((curr.yearOfDeath - curr.yearOfBirth) > (prev.yearOfDeath - prev.yearOfBirth)) ? 1 : -1;
+  });
+  // first person on list will be oldest
+  return oldest[0]; 
 }
 
 module.exports = findTheOldest
